5170bd095726d158134b33ce5904223a7b285dcc
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-06-03 10:10:52 +0500

    i learned how can i write a prgram in dart file and print a value

---
+++ lib/class 01 .dart
@@ -0,0 +1,17 @@
+
+
+
+import 'dart:io';
+
+  // in this class, I have learned how can I write a program in Dart, and as well as these topics
+  // 1. how can I create a dart file
+  // 2. how can I write main function in dart
+  // 3. how can I show a value in the console using print
+  // 4. how can I take input from the user using std.readLineSync
+  // 5. how can I show a value in the console using std.write
+
+void main () {
+  print('hello');
+  var name = stdin.readLineSync();
+  stdout.write("you entered this name : $name");
+}
