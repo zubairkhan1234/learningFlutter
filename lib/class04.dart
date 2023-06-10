ca1cd96477cfa351beb084319b9ccbb9b71a44a1
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-06-10 14:51:05 +0500

    var and dynamic keywork variable

--- lib/class04.dart
+++ lib/class04.dart
@@ -1,1 +1,53 @@
 // class 04
+
+// In this class i have learned difference in var and dynamic variable
+
+void main() {
+    // Here is creating a variable using the var keyword.
+
+    // When we create a variable using var with assigning value then it supposes
+    // type according to the value of the initial time.
+
+    // when we reassign a value with a different type of value like String to int It will not reassign, it gives an error see blew example
+
+    // var name = "zubair"; //  here create a variable with assign a value String Type.
+    // name = 7 ;       // it gives an error and not reassigning.
+
+
+    // here is creating a variable using var keyword.
+
+    // when we create a variable using var without assigning value then it supposes
+    // dynamic type because we have not assigned a value on initial time.
+
+    // when we reassign a value with a different type of value like String to int it will reassign because the variable type has been set with dynamic type, now it will not give an error see the below example.
+
+
+
+
+
+
+
+
+
+        // var section; // here we have created a variable with var keyword without assigning a variable value.
+
+        // section = "A"; // here reAssigned a value with the type of String.
+
+        // section = 2 ; // here reAssigned a value with the type of int.
+
+        // section = false; // here reAssigned a value with the type of boolean.
+
+
+        // And another way we have another type of variable name is dynamic.
+
+
+    var section; // here we have created a variable with the dynamic key without assigning a variable value.
+
+    section = "A"; // here reAssigned a value with the type of String.
+
+    section = 2 ; // here reAssigned a value with the type of int.
+
+    section = false; // here reAssigned a value with the type of boolean.
+  
+    // print(section);
+}
