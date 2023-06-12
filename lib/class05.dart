a61a9ad6ba4159fd464ecd9547c3642bb4a00160
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-06-13 00:11:06 +0500

    functions in dart

--- lib/class05.dart
+++ lib/class05.dart
@@ -1,3 +1,17 @@
 void main () {
+  var formValidationfunctions = FormValidation();
 
+   formValidationfunctions.add(5, 6);
+}
+
+
+
+class FormValidation {
+  FormValidation();
+
+  int add(int value1, int value2){
+    var sum = value1 + value2;
+    print(sum);
+    return sum;
+  }
 }
