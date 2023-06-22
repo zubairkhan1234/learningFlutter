7861cc8b88b50311bb0843454757adb503cb90bc
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-06-22 21:06:16 +0500

    learning for map

--- lib/class08.dart
+++ lib/class08.dart
@@ -25,6 +25,36 @@ void main(){
   // print(userData2);
 
 
+  // Map<String, int> user  = {
+  //   "name": "Muhammad zubair", // here is getting error due to value type is int
+  //   "rollNumber" : 12131,
+  // };
+
+  var userqdata = [
+    {
+      "name" : [1,2,1,21,2,21],
+      "user": 12
+    },
+    {
+      "name" : [1,2,1,21,2,21],
+      "user": 12
+    },{
+      "name" : [1,2,1,21,2,21],
+      "user": 12
+    },
+  ];
+  // here is getting error in blew print
+  // print(userqdata[0]['name'][0]);
+
+
+  // learning methods
+  //For each
+  // Map
+  // Where
+  // Index where
+  // Contains
+  // First where
+
   print('Is not empty ${userdata.isNotEmpty}');
   print('Is empty ${userdata.isEmpty}');
   print('length ${userdata.length}');
@@ -32,5 +62,4 @@ void main(){
   print('values ${userdata.values}');
   print('containsKey ${userdata.containsKey('productName')}');
   print('containsValue ${userdata.containsValue(2)}');
-
 }
