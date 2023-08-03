3d858a450d2fde8925e8b51aa1645bf772cb6931
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-08-03 23:28:15 +0500

    learning about OOP

---
+++ lib/class12.dart
@@ -0,0 +1,31 @@
+void main() {
+
+  Person person = Person();
+  person.mappingList();
+
+}
+
+class Person {
+  String? name ;
+  int? age ;
+
+
+  List list = ["hae", "dfjks", " dfsdf ", "fsfs "] ;
+  mappingList(){
+    print(list);
+  }
+  mappingList1(){
+
+  }
+  mappingList2(){
+
+  }
+
+}
+
+
+class Teacher {
+
+}
+
+class
