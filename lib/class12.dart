7130cfe3208ef0cbad87b57c109c9464fe74e392
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-08-08 23:52:54 +0500

    learning about OOP

--- lib/class12.dart
+++ lib/class12.dart
@@ -3,13 +3,14 @@ void main() {
   Person person = Person();
   person.mappingList();
 
+  Student  student = Student();
+  student.gettingSchoolsList();
+  student.justprinting();
 }
 
 class Person {
   String? name ;
   int? age ;
-
-
   List list = ["hae", "dfjks", " dfsdf ", "fsfs "] ;
   mappingList(){
     print(list);
@@ -17,12 +18,9 @@ class Person {
   mappingList1(){
 
   }
-
-
   mappingList2(){
 
   }
-
 }
 
 class Schools {
@@ -36,10 +34,15 @@ class Schools {
 }
 
 class Teacher extends Schools {
+  gettingSchoolsList(){
+    print(list);
+  }
 
+  justprinting(){
+    print("it is called inheritance");
+  }
 }
 
 class Student extends Teacher {
 
 }
-
